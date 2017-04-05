ec90f8bc196e1361f65dd0ad506ab581589535c3
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-05 18:23:18 -0300

    planilha nova ( subset = soroconversão)
    Foi criada, manualmente, uma nova planilha contendo apenas os pacientes que
    
    1. contém mais de uma observação
    2. trocam de status entre observações

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -1,11 +1,6 @@
 library(readxl)
-DF <- read_excel("data-raw/Cópia de Banco_AnaClaudia_16032017.xls")
 library(data.table)
+DF <- read_excel("data-raw/Cópia de Banco_AnaClaudia_16032017.xls", 2)
 DT <- data.table(DF)
-DT[`PRNT 50` == "NEG"]$`PRNT 50` <- "5.0"
-DT$`PRNT 50` <- as.numeric(DT$`PRNT 50`)
-DT[`PRNT 90` == "NEG", ]$`PRNT 90` <- "5.0"
-DT[`PRNT 90` == "neg", ]$`PRNT 90` <- "5.0"
-DT$`PRNT 90` <- as.numeric(DT$`PRNT 90`)
 
 write.csv(DT, "dataset/dados.csv", row.names = F)
