dc467e0c43639d78f2b125d13ee09bc8fe0f4116
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-05 18:13:59 -0300

    data.table para subtituir valores NEG e neg por 5, e salvar em CSV

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -1,3 +1,11 @@
 library(readxl)
 DF <- read_excel("data-raw/Cópia de Banco_AnaClaudia_16032017.xls")
-write.csv(DF, "dataset/dados.csv", row.names = F)
+library(data.table)
+DT <- data.table(DF)
+DT[`PRNT 50` == "NEG"]$`PRNT 50` <- "5.0"
+DT$`PRNT 50` <- as.numeric(DT$`PRNT 50`)
+DT[`PRNT 90` == "NEG", ]$`PRNT 90` <- "5.0"
+DT[`PRNT 90` == "neg", ]$`PRNT 90` <- "5.0"
+DT$`PRNT 90` <- as.numeric(DT$`PRNT 90`)
+
+write.csv(DT, "dataset/dados.csv", row.names = F)
