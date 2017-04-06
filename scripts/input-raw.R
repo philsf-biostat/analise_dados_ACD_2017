d4e8b6dd7242e33994fe0eda3e4928ebfe4587cc
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-06 14:00:55 -0300

    padronizar PCR negativo (==0)

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -7,5 +7,7 @@ DT$`PRNT 50` <- as.numeric(DT$`PRNT 50`)
 DT[`PRNT 90` == "NEG", ]$`PRNT 90` <- "5.0"
 DT[`PRNT 90` == "neg", ]$`PRNT 90` <- "5.0"
 DT$`PRNT 90` <- as.numeric(DT$`PRNT 90`)
+DT[PCR == "NEG"]$PCR <- "0"
+DT$PCR <- as.numeric(DT$PCR)
 
 write.csv(DT, "dataset/dados.csv", row.names = F)
