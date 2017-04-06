3cb10dde50328dbd0a2d5af69b6f1296b9c4b9be
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-06 14:01:15 -0300

    Calcular idades

--- scripts/input.R
+++ scripts/input.R
@@ -1,2 +1,7 @@
 library(data.table)
+library(lubridate)
+
 DT <- data.table(read.csv("dataset/dados.csv"))
+
+## Idade
+DT$Idade <- DT[, .( Idade = year(as.period(interval(DN, Data_coleta_Rexames))) ) ]
