b61d9297da8f93cbe1788f443acb7fddd90bb371
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-18 10:27:42 -0300

    Gráficos do PRNT90 em comparação com PRNT50

--- scripts/exploratoria.R
+++ scripts/exploratoria.R
@@ -1,5 +1,9 @@
 library(ggplot2)
 ggplot(DT, aes(Data_coleta_Rexames, PRNT.50)) + geom_line(aes(color = Febril, group = reg))
+ggplot(DT, aes(Data_coleta_Rexames, PRNT.90)) + geom_line(aes(color = Febril, group = reg))
+
 ggplot(DT, aes(Data_coleta_Rexames, PRNT.50)) + geom_line(aes(color = Exantema, group = reg))
+ggplot(DT, aes(Data_coleta_Rexames, PRNT.90)) + geom_line(aes(color = Exantema, group = reg))
 
 ggplot(DT, aes(Data_coleta_Rexames, PRNT.50)) + geom_line(aes(color = reg, group = reg))
+ggplot(DT, aes(Data_coleta_Rexames, PRNT.90)) + geom_line(aes(color = reg, group = reg))
