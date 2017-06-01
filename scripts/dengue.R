dfe2e752db6eb3d19a967bb45fa30e7c9fd94989
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-01 17:18:13 -0300

    log10 transform

--- scripts/dengue.R
+++ scripts/dengue.R
@@ -9,14 +9,15 @@ DT <- DT[, lapply(.SD, data.replace, 10, "<10")]
 DT <- DT[, lapply(.SD, as.numeric)]
 DT$`ID de registro` <- factor(DT$`ID de registro`)
 
-cor.test(DT$`sangue de cordão`, DT$`soro materno`, method = "spearman" ) # r = 0.1821501, p = 0.1712
-cor.test(DT$`sangue de cordão__1`, DT$`soro materno__1`, method = "spearman") # r = -0.04581803, p = 0.7327
-cor.test(DT$`sangue de cordão__2`, DT$`soro materno__2`, method = "spearman") # r = 0.01408245, p = 0.9164
-cor.test(DT$`sangue de cordão__3`, DT$`soro materno__3`, method = "spearman") # r = -0.005848243, p = 0.9652
+cor.test(log10(DT$`sangue de cordão`), log10(DT$`soro materno`)) # r = 0.1821501, p = 0.1712
+cor.test(log10(DT$`sangue de cordão__1`), log10(DT$`soro materno__1`)) # r = -0.04581803, p = 0.7327
+cor.test(log10(DT$`sangue de cordão__2`), log10(DT$`soro materno__2`)) # r = 0.01408245, p = 0.9164
+cor.test(log10(DT$`sangue de cordão__3`), log10(DT$`soro materno__3`)) # r = -0.005848243, p = 0.9652
 
 DT2 <- DT[, lapply(.SD, data.replace, NA, c(250, 10))]
 
-cor.test(DT2$`sangue de cordão`, DT2$`soro materno`, method = "spearman") # r = 0.1821501, p = 0.1712
-cor.test(DT2$`sangue de cordão__1`, DT2$`soro materno__1`, method = "spearman") # r = -0.04581803, p = 0.7327
-cor.test(DT2$`sangue de cordão__2`, DT2$`soro materno__2`, method = "spearman") # r = 0.01408245, p = 0.9164
-cor.test(DT2$`sangue de cordão__3`, DT2$`soro materno__3`, method = "spearman") # r = -0.005848243, p = 0.9652
+cor.test(log10(DT2$`sangue de cordão`), log10(DT2$`soro materno`)) # r = 0.1821501, p = 0.1712
+cor.test(log10(DT2$`sangue de cordão__1`), log10(DT2$`soro materno__1`)) # r = -0.04581803, p = 0.7327
+cor.test(log10(DT2$`sangue de cordão__2`), log10(DT2$`soro materno__2`)) # r = 0.01408245, p = 0.9164
+cor.test(log10(DT2$`sangue de cordão__3`), log10(DT2$`soro materno__3`)) # r = -0.005848243, p = 0.9652
+
