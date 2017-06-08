beda2117012a57e7725d2c3753bc3041b44a8ba4
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-08 17:55:16 -0300

    correlação de spearman

--- scripts/dengue.R
+++ scripts/dengue.R
@@ -14,12 +14,18 @@ cor.test(log10(DT$sc.d2), log10(DT$sm.d2)) # r = -0.04581803, p = 0.7327
 cor.test(log10(DT$sc.d3), log10(DT$sm.d3)) # r = 0.01408245, p = 0.9164
 cor.test(log10(DT$sc.d4), log10(DT$sm.d4)) # r = -0.005848243, p = 0.9652
 
+cor.test(DT$sc.d1, DT$sm.d1, method = "spearman") # r = 0.1821501, p = 0.1712
+cor.test(DT$sc.d2, DT$sm.d2, method = "spearman") # r = -0.04581803, p = 0.7327
+cor.test(DT$sc.d3, DT$sm.d3, method = "spearman") # r = 0.01408245, p = 0.9164
+cor.test(DT$sc.d4, DT$sm.d4, method = "spearman") # r = -0.005848243, p = 0.9652
+
 DT2 <- DT[, lapply(.SD, data.replace, NA, c(250, 10))]
 
 cor.test(log10(DT2$sc.d1), log10(DT2$sm.d1)) # r = 0.1821501, p = 0.1712
 cor.test(log10(DT2$sc.d2), log10(DT2$sm.d2)) # r = -0.04581803, p = 0.7327
 cor.test(log10(DT2$sc.d3), log10(DT2$sm.d3)) # r = 0.01408245, p = 0.9164
 cor.test(log10(DT2$sc.d4), log10(DT2$sm.d4)) # r = -0.005848243, p = 0.9652
+
 library(ggplot2)
 
 ggplot(DT, aes(sm.d1, sc.d1)) + geom_point() + scale_x_log10() + scale_y_log10() +geom_jitter() + geom_smooth(method = "lm", se = F)
