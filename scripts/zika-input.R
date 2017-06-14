a52694ad1f312344079c5449936dd1f2ff99c308
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-14 18:35:40 -0300

    banco de ZIKA sem padronização

--- scripts/zika-input.R
+++ scripts/zika-input.R
@@ -1,16 +1,16 @@
 source("scripts/data-replace.R")
-ZK <- read_excel("dataset/Banco AC ZIKA PRNT 50 x 90.xlsx")
+ZK <- read_excel("dataset/Banco AC ZIKA PRNT 50 x 90.xlsx", na = "ND")
 ZK <- data.table(ZK)
 
 ZK$reg <- factor(ZK$reg)
 ZK$Febril <- factor(ZK$Febril)
 ZK$Exantema <- factor(ZK$Exantema)
-ZK$`PRNT 50` <- as.numeric(data.replace(ZK$`PRNT 50`, 5, "NEG"))
-ZK$`PRNT 50 D1` <- as.numeric(data.replace(ZK$`PRNT 50 D1`, 5, "NEG"))
-ZK$`PRNT 50 D4` <- as.numeric(data.replace(ZK$`PRNT 50 D4`, 5, "NEG"))
-ZK$`PRNT 90` <- as.numeric(data.replace(ZK$`PRNT 90`, 5, "NEG"))
-ZK$`PRNT 90 D1` <- as.numeric(data.replace(ZK$`PRNT 90 D1`, 5, "NEG"))
-ZK$`PRNT 90 D4` <- as.numeric(data.replace(ZK$`PRNT 90 D4`, 5, "NEG"))
+ZK$`PRNT 50` <- as.numeric(data.replace(ZK$`PRNT 50`, 5, c("NEG", "neg")))
+ZK$`PRNT 50 D1` <- as.numeric(data.replace(ZK$`PRNT 50 D1`, 5, c("NEG", "neg")))
+ZK$`PRNT 50 D4` <- as.numeric(data.replace(ZK$`PRNT 50 D4`, 5, c("NEG", "neg")))
+ZK$`PRNT 90` <- as.numeric(data.replace(ZK$`PRNT 90`, 5, c("NEG", "neg")))
+ZK$`PRNT 90 D1` <- as.numeric(data.replace(ZK$`PRNT 90 D1`, 5, c("NEG", "neg")))
+ZK$`PRNT 90 D4` <- as.numeric(data.replace(ZK$`PRNT 90 D4`, 5, c("NEG", "neg")))
 
 ZK.2015 <- ZK[Data_coleta_Rexames >= "2015-01-01" & Data_coleta_Rexames <= "2015-12-31"]
 ZK.2016 <- ZK[Data_coleta_Rexames >= "2016-01-01" & Data_coleta_Rexames <= "2016-12-31"]
