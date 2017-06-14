7b9cd67680258a31758ff7b461e8490fff278318
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-14 17:32:54 -0300

    dados de zika por ano

--- scripts/zika-input.R
+++ scripts/zika-input.R
@@ -11,3 +11,6 @@ ZK$`PRNT 50 D4` <- as.numeric(data.replace(ZK$`PRNT 50 D4`, 5, "NEG"))
 ZK$`PRNT 90` <- as.numeric(data.replace(ZK$`PRNT 90`, 5, "NEG"))
 ZK$`PRNT 90 D1` <- as.numeric(data.replace(ZK$`PRNT 90 D1`, 5, "NEG"))
 ZK$`PRNT 90 D4` <- as.numeric(data.replace(ZK$`PRNT 90 D4`, 5, "NEG"))
+
+ZK.2015 <- ZK[Data_coleta_Rexames >= "2015-01-01" & Data_coleta_Rexames <= "2015-12-31"]
+ZK.2016 <- ZK[Data_coleta_Rexames >= "2016-01-01" & Data_coleta_Rexames <= "2016-12-31"]
