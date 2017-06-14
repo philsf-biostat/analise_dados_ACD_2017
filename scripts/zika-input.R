daa8b627f87d474beee7a51335db7d5b73296a5b
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-14 17:26:48 -0300

    dados ZIKA padronizados

---
+++ scripts/zika-input.R
@@ -0,0 +1,13 @@
+source("scripts/data-replace.R")
+ZK <- read_excel("dataset/Banco AC ZIKA PRNT 50 x 90.xlsx")
+ZK <- data.table(ZK)
+
+ZK$reg <- factor(ZK$reg)
+ZK$Febril <- factor(ZK$Febril)
+ZK$Exantema <- factor(ZK$Exantema)
+ZK$`PRNT 50` <- as.numeric(data.replace(ZK$`PRNT 50`, 5, "NEG"))
+ZK$`PRNT 50 D1` <- as.numeric(data.replace(ZK$`PRNT 50 D1`, 5, "NEG"))
+ZK$`PRNT 50 D4` <- as.numeric(data.replace(ZK$`PRNT 50 D4`, 5, "NEG"))
+ZK$`PRNT 90` <- as.numeric(data.replace(ZK$`PRNT 90`, 5, "NEG"))
+ZK$`PRNT 90 D1` <- as.numeric(data.replace(ZK$`PRNT 90 D1`, 5, "NEG"))
+ZK$`PRNT 90 D4` <- as.numeric(data.replace(ZK$`PRNT 90 D4`, 5, "NEG"))
