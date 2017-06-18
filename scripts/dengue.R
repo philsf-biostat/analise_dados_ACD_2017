472ee035c921f2411f777cc2d9d7adeb83773cd1
Felipe Figueiredo <philsf79@gmail.com>
2017-06-18 18:08:29 -0300

    fix: não tratar a coluna 2 como numerica

--- scripts/dengue.R
+++ scripts/dengue.R
@@ -6,7 +6,7 @@ DT <- read_excel("dataset/concordancia dengue soro materno e sangue de cordao.xl
 DT <- as.data.table(DT)
 DT <- DT[, lapply(.SD, data.replace, 250, ">250")]
 DT <- DT[, lapply(.SD, data.replace, 10, "<10")]
-DT <- DT[, lapply(.SD, as.numeric)]
+DT <- DT[, lapply(.SD, as.numeric), .SDcols = c(1,3:10)]
 setkey(DT, "ID de registro")
 DT$`ID de registro` <- factor(DT$`ID de registro`)
 
