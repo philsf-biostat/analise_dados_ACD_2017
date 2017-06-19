35f8a16041fb62679a82771c777c76f774ca32e7
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-06-19 15:58:13 -0300

    Gerenciar dados de associação. Closes #18
    - <10 e 10 substituídos por 5
    - >1250 e 1250 substituídos por 2500

--- scripts/dengue-assoc-input.R
+++ scripts/dengue-assoc-input.R
@@ -4,8 +4,8 @@ source("scripts/data-replace.R")
 
 DT <- read_excel("dataset/concordancia dengue soro materno e sangue de cordao.xlsx")
 DT <- as.data.table(DT)
-DT <- DT[, lapply(.SD, data.replace, 250, ">250")]
-DT <- DT[, lapply(.SD, data.replace, 10, "<10")]
+DT <- DT[, lapply(.SD, data.replace, 2500, c(">1250", 1250))]
+DT <- DT[, lapply(.SD, data.replace, 5, c("<10", 10))]
 DT <- DT[, lapply(.SD, as.numeric), .SDcols = c(1,3:10)]
 setkey(DT, "ID de registro")
 DT$`ID de registro` <- factor(DT$`ID de registro`)
