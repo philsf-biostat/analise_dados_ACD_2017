681aa9c3d3e9249a7f08d69140b4687062a93c6e
Felipe Figueiredo <philsf79@gmail.com>
2017-06-18 21:46:08 -0300

    cinética: código obsoleto comentado

--- scripts/dengue-cinetica.R
+++ scripts/dengue-cinetica.R
@@ -1,6 +1,8 @@
 # Ler BB1, BB2, BB3, BB4
 source("scripts/dengue-cinetica-input.R")
 
+# obsoleto ----------------------------------------------------------------
+
 # la <- data.table(
 #   ID = c(BB.not$`ID de registro`,
 #          BB.not$`ID de registro`,
@@ -21,26 +23,26 @@ source("scripts/dengue-cinetica-input.R")
 # la[`soroconversão 1` == "não" & `soroconversão 2` == "não" & `soroconversão 3` == "não"]
 # la$Idade.cat <- cut(la$Idade, breaks = c(2,4,6,8,10,12, Inf), right = T)
 
-png("figuras/dengue-cinetica.png")
-par(mfrow = c(1,2))
-
-with(BB1, plot(Idade.cat, Titulo, log = "y"))
-abline(h = 50, lwd = 2, col = "red")
-title("Todos", ylab = "Título", xlab = "Idade (meses)")
-
-with(BB1[soroconversao == "não"], plot(Idade.cat, Titulo, log = "y"))
-abline(h = 50, lwd = 2, col = "red")
-title("Sem soroconversao", ylab = "Título", xlab = "Idade (meses)")
-
-dev.off()
+# png("figuras/dengue-cinetica.png")
+# par(mfrow = c(1,2))
+# 
+# with(BB1, plot(Idade.cat, Titulo, log = "y"))
+# abline(h = 50, lwd = 2, col = "red")
+# title("Todos", ylab = "Título", xlab = "Idade (meses)")
+# 
+# with(BB1[soroconversao == "não"], plot(Idade.cat, Titulo, log = "y"))
+# abline(h = 50, lwd = 2, col = "red")
+# title("Sem soroconversao", ylab = "Título", xlab = "Idade (meses)")
+# 
+# dev.off()
 
 # ggplot(BB1[soroconversao == "não"], aes(Idade.cat, Titulo)) + geom_boxplot() + geom_hline(yintercept = 50, col = "red", size = 1.5) + scale_y_log10()
-png("figuras/soroconv-dengue1.png", 500, 800)
-par(mfrow = c(3,1))
-barplot(with(BB1[soroconversao == "não"], table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
-title(("não"))
-barplot(with(BB1[soroconversao == "sim"], table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
-title(("sim"))
-barplot(with(BB1, table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
-title("xerau")
-dev.off()
+# png("figuras/soroconv-dengue1.png", 500, 800)
+# par(mfrow = c(3,1))
+# barplot(with(BB1[soroconversao == "não"], table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
+# title(("não"))
+# barplot(with(BB1[soroconversao == "sim"], table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
+# title(("sim"))
+# barplot(with(BB1, table(Titulo.cat, Idade.cat )), beside = T, legend.text = T, ylim = c(0, 60))
+# title("xerau")
+# dev.off()
