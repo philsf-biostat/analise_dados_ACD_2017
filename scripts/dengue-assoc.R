af0d34ba877669207d30fa171877890260fe6672
Felipe Figueiredo <philsf79@gmail.com>
2017-06-18 22:13:30 -0300

    Usar apenas correlação de spearman para associação. Closes #1

--- scripts/dengue-assoc.R
+++ scripts/dengue-assoc.R
@@ -1,20 +1,14 @@
 source("scripts/dengue-assoc-input.R")
-cor.test(log10(DT$sc.d1), log10(DT$sm.d1)) # r = 0.1821501, p = 0.1712
-cor.test(log10(DT$sc.d2), log10(DT$sm.d2)) # r = -0.04581803, p = 0.7327
-cor.test(log10(DT$sc.d3), log10(DT$sm.d3)) # r = 0.01408245, p = 0.9164
-cor.test(log10(DT$sc.d4), log10(DT$sm.d4)) # r = -0.005848243, p = 0.9652
+# cor.test(log10(DT$sc.d1), log10(DT$sm.d1)) # r = 0.1821501, p = 0.1712
+# cor.test(log10(DT$sc.d2), log10(DT$sm.d2)) # r = -0.04581803, p = 0.7327
+# cor.test(log10(DT$sc.d3), log10(DT$sm.d3)) # r = 0.01408245, p = 0.9164
+# cor.test(log10(DT$sc.d4), log10(DT$sm.d4)) # r = -0.005848243, p = 0.9652
 
-cor.test(DT$sc.d1, DT$sm.d1, method = "spearman") # r = 0.5262571, p = <0.001
-cor.test(DT$sc.d2, DT$sm.d2, method = "spearman") # r = 0.2761045, p = 0.0359
-cor.test(DT$sc.d3, DT$sm.d3, method = "spearman") # r = 0.5174988, p = <0.001
-cor.test(DT$sc.d4, DT$sm.d4, method = "spearman") # r = 0.5062238, p = <0.001
+cor.test(DT$sc.d1, DT$sm.d1, method = "spearman") # rho = 0.5262571, p = <0.001
+cor.test(DT$sc.d2, DT$sm.d2, method = "spearman") # rho = 0.2761045, p = 0.0359
+cor.test(DT$sc.d3, DT$sm.d3, method = "spearman") # rho = 0.5174988, p = <0.001
+cor.test(DT$sc.d4, DT$sm.d4, method = "spearman") # rho = 0.5062238, p = <0.001
 
-DT2 <- DT[, lapply(.SD, data.replace, NA, c(250, 10))]
-
-cor.test(log10(DT2$sc.d1), log10(DT2$sm.d1)) # r = 0.1821501, p = 0.1712
-cor.test(log10(DT2$sc.d2), log10(DT2$sm.d2)) # r = -0.04581803, p = 0.7327
-cor.test(log10(DT2$sc.d3), log10(DT2$sm.d3)) # r = 0.01408245, p = 0.9164
-cor.test(log10(DT2$sc.d4), log10(DT2$sm.d4)) # r = -0.005848243, p = 0.9652
 
 library(ggplot2)
 
