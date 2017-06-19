dfbc943ed22aa18a5fe3114482465dba1b4f5ed7
Felipe Figueiredo <philsf79@gmail.com>
2017-06-18 21:48:04 -0300

    gráficos de cinética movidos para um único script

--- scripts/dengue-cinetica.R
+++ scripts/dengue-cinetica.R
@@ -1,6 +1,37 @@
 # Ler BB1, BB2, BB3, BB4
 source("scripts/dengue-cinetica-input.R")
 
+library(ggplot2)
+
+kin.plot <- function(dataframe) {
+  ggplot(dataframe, aes(Idade, Titulo,
+                        color = soroconversao,
+                        group = soroconversao,
+                        shape = soroconversao)) +
+    labs(color = "Soroconversão", shape = "Soroconversão") +
+    scale_y_log10() +
+    scale_x_continuous(breaks = seq(0,12,2)) +
+    geom_hline(yintercept = c(10, 1250), linetype = "dashed") +
+    # geom_boxplot() +
+    geom_hline(yintercept = 50, col = "red") +
+    geom_jitter(width = .1, alpha = .3) +
+    geom_smooth(method = "lm", size = .7) +
+    stat_summary(fun.y = geomean, geom = "point", mapping = aes(y = Titulo), size = 2) +
+    scale_color_brewer(palette="Dark2") +
+    theme(plot.subtitle = element_text(hjust = 0.5))
+}
+
+bb1 <- kin.plot(BB1) + labs(title = "A", subtitle = "Cinética de DENV1")
+ggsave("figuras/cinetica-dengue1.png")
+bb2 <- kin.plot(BB2) + labs(title = "B", subtitle = "Cinética de DENV2")
+ggsave("figuras/cinetica-dengue2.png")
+bb3 <- kin.plot(BB3) + labs(title = "C", subtitle = "Cinética de DENV3")
+ggsave("figuras/cinetica-dengue3.png")
+bb4 <- kin.plot(BB4) + labs(title = "D", subtitle = "Cinética de DENV4")
+ggsave("figuras/cinetica-dengue4.png")
+
+
+
 # obsoleto ----------------------------------------------------------------
 
 # la <- data.table(
