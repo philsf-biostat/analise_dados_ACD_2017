3870be29195b6cadb2e1ba616bb731a0f61f3cf2
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-07-18 17:13:43 -0300

    cálculo do N total movido para o início do script

--- scripts/zika-reprod.R
+++ scripts/zika-reprod.R
@@ -1,4 +1,11 @@
 source("scripts/zika-input.R")
+
+# N total (com dados de DENV1 e DENV4, ZIKA positivos, excluindo 2017)
+N <- length((ZK[Data_coleta_Rexames !=2017 &
+                  `PRNT 90`>=50 & !is.na(`PRNT 50 D1`) &
+                  !is.na(`PRNT 50 D4`)
+                ,.(reg, Data_coleta_Rexames,  `PRNT 90`, `PRNT 50 D1`, `PRNT 50 D4`)]$reg))
+
 ZD1 <- ZK[Data_coleta_Rexames != 2017 &
             `PRNT 90` >= 50 &
             `PRNT 90`/`PRNT 50 D1` >= 3,
@@ -11,7 +18,6 @@ ZD4 <- ZK[Data_coleta_Rexames != 2017 &
 ZD1$`PRNT 50 D1` <- data.replace(ZD1$`PRNT 50 D1`, "<1:10", 5)
 ZD4$`PRNT 50 D4` <- data.replace(ZD4$`PRNT 50 D4`, "<1:10", 5)
 
-N <- length((ZK[Data_coleta_Rexames !=2017 & `PRNT 90`>=50 & !is.na(`PRNT 50 D1`) & !is.na(`PRNT 50 D4`),.(reg, Data_coleta_Rexames,  `PRNT 90`, `PRNT 50 D1`, `PRNT 50 D4`)]$reg))
 n1 <- length((ZD1$reg))
 n4 <- length((ZD4$reg))
 
