60e67b5a7666088f4af08cf15d9f62293078d83e
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-07-18 17:00:13 -0300

    fix: >= ao corte

--- scripts/zika-reprod.R
+++ scripts/zika-reprod.R
@@ -1,11 +1,11 @@
 source("scripts/zika-input.R")
 ZD1 <- ZK[Data_coleta_Rexames != 2017 &
-            `PRNT 90`/`PRNT 50 D1` > 3,
             `PRNT 90` >= 50 &
+            `PRNT 90`/`PRNT 50 D1` >= 3,
           .(reg, Data_coleta_Rexames,  `PRNT 90`, `PRNT 50 D1`)]
 ZD4 <- ZK[Data_coleta_Rexames != 2017 &
-            `PRNT 90`/`PRNT 50 D4` > 3,
             `PRNT 90` >= 50 &
+            `PRNT 90`/`PRNT 50 D4` >= 3,
           .(reg, Data_coleta_Rexames,  `PRNT 90`, `PRNT 50 D4`)]
 
 ZD1$`PRNT 50 D1` <- data.replace(ZD1$`PRNT 50 D1`, "<1:10", 5)
