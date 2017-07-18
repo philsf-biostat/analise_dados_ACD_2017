23f653bffa90a551528c9f0a3b9991e0d37740eb
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-07-18 17:00:35 -0300

    N de amostras total e n de positivos (inclui registros repetidos)

--- scripts/zika-reprod.R
+++ scripts/zika-reprod.R
@@ -10,3 +10,10 @@ ZD4 <- ZK[Data_coleta_Rexames != 2017 &
 
 ZD1$`PRNT 50 D1` <- data.replace(ZD1$`PRNT 50 D1`, "<1:10", 5)
 ZD4$`PRNT 50 D4` <- data.replace(ZD4$`PRNT 50 D4`, "<1:10", 5)
+
+N <- length((ZK[Data_coleta_Rexames !=2017 & `PRNT 90`>=50 & !is.na(`PRNT 50 D1`) & !is.na(`PRNT 50 D4`),.(reg, Data_coleta_Rexames,  `PRNT 90`, `PRNT 50 D1`, `PRNT 50 D4`)]$reg))
+n1 <- length((ZD1$reg))
+n4 <- length((ZD4$reg))
+
+binom.exact(n1,N)
+binom.exact(n4,N)
